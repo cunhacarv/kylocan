3826736265868fbc1cd68ca24edfea34d637fb45
cunhacarv <45003653+cunhacarv@users.noreply.github.com>
2018-11-27 11:16:13 -0200

    Add files via upload

---
+++ dinho.php
@@ -0,0 +1,15 @@
+<?php
+$x = $_GET[“num1”];
+$y = $_GET[“num2”];
+$op = $_GET[“operacao”];
+if($op==”soma”)
+$z = $x + $y;
+elseif($op==”subtracao”)
+$z = $x – $y;
+elseif($op==”multiplicacao”)
+$z = $x*$y;
+elseif($op==”divisao”)
+$z = $x/$y;
+else
+$z = pow($x, $y);
+echo “O resultado e: $z”;
